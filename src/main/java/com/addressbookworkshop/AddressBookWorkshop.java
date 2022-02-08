0b2e5eba380e511cef83e76156bb8c0009fe4b30
NayanTr09 <nayantripathi42@gmail.com>
2022-02-08 08:23:56 +0530

    Display Welcome message

---
+++ src/main/java/com/addressbookworkshop/AddressBookWorkshop.java
@@ -0,0 +1,12 @@
+/* @author : Nayan Tripathi
+@Date : 08/02/2022
+Workshop Program : AddressBook
+ */
+
+package com.addressbookworkshop;
+
+public class AddressBookWorkshop {
+    public static void main(String args[]) {
+        System.out.println("This is a Address Book! "); //Display Welcome message
+    }
+}
